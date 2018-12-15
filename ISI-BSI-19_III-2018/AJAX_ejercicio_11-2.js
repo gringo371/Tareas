f14666811e64a6d302f67014b94685f24b59bc8c
Angel <piedra5739821@hotmail.com>
2018-12-15 02:41:40 -0600

    Add files via upload

---
+++ ISI-BSI-19_III-2018/AJAX_ejercicio_11-2.js
@@ -0,0 +1,40 @@
+//Ejercicio 11 de la practica de segunda semana
+//Autor: Luis Angel Piedra
+
+var num1;
+var num2;
+var resultado;
+
+
+$(function (){
+
+	var input1 = ('#orders');
+
+	input1.val = "54";
+
+
+
+});
+
+/*
+function crearAjaxPedido(){
+	if(window.XMLHttpRequest()){
+		return new XMLHttpRequest();
+	}
+	else{
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+function alCargar(){
+	num1 = document.getElementById("num1");
+	num2 = document.getElementById("num2");
+	resultado = document.getElementById("resultado");
+}
+
+function cuandoCambianLosNumeros(){
+	var numero1 = num1.value;
+	var numero2 = num2.value;
+	resultado.value = numero1 + numero2;
+}
+*/
